38dd0f8b8a022462a8f2a9924d6109b2b5d4983b
Angelito1210 <angelmorenomarquez19@gmail.com>
2026-03-30 00:59:25 +0200

    feat: primer proyecto real en AWS S3 con Terraform

--- main.tf
+++ main.tf
@@ -1,4 +1,57 @@
-resource "local_file" "bienvenida" {
-  content  = "Confirmado: He instalado Terraform y estoy a un paso de aprender más para mi carrera profesional"
-  filename = "exito.txt"
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+resource "aws_s3_bucket" "portfolio" {
+  bucket = "angel-devops-portfolio-${random_string.suffix.result}"
+}
+
+resource "aws_s3_bucket_website_configuration" "portfolio" {
+  bucket = aws_s3_bucket.portfolio.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "portfolio" {
+  bucket = aws_s3_bucket.portfolio.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "portfolio" {
+  bucket = aws_s3_bucket.portfolio.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Sid       = "PublicReadGetObject"
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.portfolio.arn}/*"
+    }]
+  })
+}
+
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.portfolio.id
+  key          = "index.html"
+  source       = "index.html"
+  content_type = "text/html"
+  etag         = filemd5("index.html")
+}
+
+output "website_url" {
+  value = "http://${aws_s3_bucket.portfolio.bucket}.s3-website-eu-west-1.amazonaws.com"
 }
